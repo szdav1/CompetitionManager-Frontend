5ec004b97d498fa9ebf6df0f8f0090894a9ecbdd
szxbodavee23 <davedszabo@gmail.com>
2025-02-19 17:16:51 +0100

    Implement basic GetCompetitor function.

--- src/components/Competitors/GetCompetitor.js
+++ src/components/Competitors/GetCompetitor.js
@@ -7,35 +7,59 @@ import TextField from '@mui/material/TextField';
 import AddIcon from '@mui/icons-material/Add';
 import Button from '@mui/material/Button';
 import SearchIcon from '@mui/icons-material/Search';
+import { set } from "react-hook-form";
+
 function GetCompetitor(){
-    const [competitor, setCompetitor] = useState(null);
+    
+    let [stateCompetitors, setStateCompetitors] = useState(null);
     const [open, setOpen] = useState(false);
-    const [id, setId] = useState("");
+    const [stateId, setStateId] = useState("");
+    const [stateName, setStateName] = useState("");
+    const [stateClub, setStateClub] = useState("");
+    const [stateBirthDate, setStateBirthDate] = useState("");
+    let [stateCompetitor, setStateCompetitor] = useState(null);
 
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    
-
-    const fetchCompetitor = async (id) => {
-        try {
-            const response = await axios.get(`http://localhost:8080/api/competitor/${id}`);
-            setCompetitor(response.data);
+    const fetchAllCompetitor = () =>{
+        axios.get("/api/competitor/")
+        .then(response => {
+            setStateCompetitors(response.data);
+        })
+        
+    }
+    fetchAllCompetitor();
+    const filterCompetitorByName = () =>{
+        stateCompetitors.map((competitor) => {
+            if(competitor.name === stateName){
+                stateCompetitor = competitor;
+                setStateCompetitor(stateCompetitor);
+            }
+        })
+    }
+    const getCompetitorPlacementsById = () =>{
+        axios.get(`/api/placements/${stateCompetitor.id}`).then(response => {
             console.log(response.data);
-        } catch (error) {
-            console.error("Error fetching competitor:", error);
-        }
-    };
+        })
+    }
+
+  
+
 
     return (
         <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
             <Button variant="contained" startIcon={<AddIcon />} onClick={handleOpen}>Get Competitor</Button>
             <Modal open={open} onClose={handleClose}>
                 <Box sx={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'white', boxShadow: 24, p: 4, display: 'flex', flexDirection: 'column', alignItems: 'center', borderRadius: 5}}>
-                    <TextField id="outlined-basic" label="Competitor ID" variant="outlined" value={id} onChange={(e) => setId(e.target.value)} />
+                    <TextField id="outlined-basic" label="Name" variant="outlined" value={stateName} onChange={(e) => setStateName(e.target.value)} />
+                    {/* <TextField id="outlined-basic" label="Club" variant="outlined" value={club} onChange={(e) => setStateClub(e.target.value)} />
+                    <TextField id="outlined-basic" label="Birth Date" variant="outlined" value={birthDate} onChange={(e) => setStateBirthDate(e.target.value)} /> */}
                     <br />
                     <Button variant="contained" startIcon={<SearchIcon />} onClick={() => {
-                        fetchCompetitor(id);
                         handleClose();
+                        fetchAllCompetitor();
+                        filterCompetitorByName();
+                        getCompetitorPlacementsById();
                     }}>Query</Button>
                     <Button variant="contained" color="error" onClick={handleClose}>Close</Button>
                 </Box>
