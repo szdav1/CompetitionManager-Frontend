0577486de3278db758600755eafc21cb91bf8692
Szabó Dávid <davedszabo@gmail.com>
2025-03-28 08:16:25 +0100

    Safety save upload

--- src/components/Competitors/GetCompetitor.js
+++ src/components/Competitors/GetCompetitor.js
@@ -8,6 +8,7 @@ import AddIcon from '@mui/icons-material/Add';
 import Button from '@mui/material/Button';
 import SearchIcon from '@mui/icons-material/Search';
 import { set } from "react-hook-form";
+import Alert from "@mui/material/Alert"
 import ListPlacements from "../ListPlacements/ListPlacements";
 
 function GetCompetitor(){
@@ -24,12 +25,13 @@ function GetCompetitor(){
     const handleClose = () => setOpen(false);
     
     const fetchAllCompetitor = async () =>{
-        let response = await axios.get("/api/competitor/")
-        setStateCompetitors(response.data);
+        let response = await axios.get("/api/competitor/").catch((error) => {alert("Kérem indítsa el a szervert!")});
+            setStateCompetitors(response.data);
+       
         
     }
     useEffect(() => {
-        fetchAllCompetitor();
+            fetchAllCompetitor();
     }, [])
     const filterCompetitorByName = () =>{
         stateCompetitors.map((competitor) => {
@@ -40,16 +42,21 @@ function GetCompetitor(){
         })
     }
     const getCompetitorPlacementsById = async () =>{
-        let response = await axios.get(`/api/placements/${stateCompetitor.id}`)
+        let response = await axios.get(`/api/placements/${stateCompetitor.id}`).catch((error) => {alert("Kérem indítsa el a szervert!")});
         statePlacements = response.data;
         setStatePlacements(statePlacements);
     }
     const clickQuery = async () =>{
+        try{
         handleClose();
         await fetchAllCompetitor();
         filterCompetitorByName();
         await getCompetitorPlacementsById();
     }
+    catch(error){
+        alert("Nem sikerült a versenyzők lekérdezése!");
+    }
+}
 
   
 
@@ -62,15 +69,16 @@ function GetCompetitor(){
                 
                 <Modal open={open} onClose={handleClose}>
                     <Box sx={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'white', boxShadow: 24, p: 4, display: 'flex', flexDirection: 'column', alignItems: 'center', borderRadius: 5}}>
-                        <TextField id="outlined-basic" label="Name" variant="outlined" value={stateName} onChange={(e) => setStateName(e.target.value)} />
-                        <TextField id="outlined-basic" label="Club" variant="outlined" value={stateClub} onChange={(e) => setStateClub(e.target.value)} />
-                        <TextField id="outlined-basic" label="Birth Date" variant="outlined" value={stateBirthDate} onChange={(e) => setStateBirthDate(e.target.value)} />
+                        <TextField required id="outlined-basic" label="Name" variant="outlined" value={stateName} onChange={(e) => setStateName(e.target.value)} />
+                        <TextField required id="outlined-basic" label="Club" variant="outlined" value={stateClub} onChange={(e) => setStateClub(e.target.value)} />
+                        <TextField required id="outlined-basic" label="Birth Date" variant="outlined" value={stateBirthDate} onChange={(e) => setStateBirthDate(e.target.value)} />
                         <br />
                         <Button variant="contained" startIcon={<SearchIcon />}  onClick={ async () => {
                             await clickQuery();
                             console.log(statePlacements);
                         }}>Query</Button>
                         <Button variant="contained" color="error" onClick={handleClose}>Close</Button>
+                        <Alert severity="info">Please fill out the fields!</Alert>
                     </Box>
                 </Modal>
             </div>
